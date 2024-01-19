cbebdb026c736b785edd52d0bcbf3ca41a35aa9a
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-01-19 14:35:15 +0200

    build tab bar view

--- lib/features/presentation/screens/bottom_navigation_bar_screens/profile_screen/screens/orders_screen.dart
+++ lib/features/presentation/screens/bottom_navigation_bar_screens/profile_screen/screens/orders_screen.dart
@@ -1,13 +1,21 @@
+import 'dart:math';
+
+import 'package:agriplant/core/utils/enums/order_status.dart';
+import 'package:agriplant/features/data/orders.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final tabs = ["Processing", "Picking", "Shipping", "Delivered"];
+    // final tabs = ["Processing", "Picking", "Shipping", "Delivered"];
+    final tabs = OrderStatus.values.map((e) => e.name).toList();
     return DefaultTabController(
       length: 4,
+      initialIndex: 0,
       child: Scaffold(
         appBar: AppBar(
           title: const Text('My orders '),
@@ -16,13 +24,29 @@ class OrdersScreen extends StatelessWidget {
             tabs: List.generate(
               tabs.length,
               (index) => Tab(
-                text: tabs[index],
+                text: '${tabs[index]} ${Random().nextInt(10)}',
               ),
             ),
           ),
         ),
-        body: const Center(
-          child: Text('Orders Screen'),
+        body: TabBarView(
+          children: List.generate(
+            tabs.length,
+            (index) {
+              final filteredOrders = orders
+                  .where(
+                      (element) => element.status == OrderStatus.values[index])
+                  .toList();
+              return ListView.separated(
+                itemBuilder: (context, index) {
+                  return OrderItem(order: filteredOrders[index],);
+                },
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 10),
+                itemCount: filteredOrders.length,
+              );
+            },
+          ),
         ),
       ),
     );
---
+++ lib/features/presentation/screens/bottom_navigation_bar_screens/profile_screen/widgets/order_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../data/models/order.dart';
+
+class OrderItem extends StatelessWidget {
+  const OrderItem({super.key , required this.order});
+  final Order order ;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
